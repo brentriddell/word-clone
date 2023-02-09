a58e24ed0d4a2ea4af8a22e2764b9a71ef470aad
Brent Riddell <brentriddell@gmail.com>
2023-02-09 14:27:34 +0000

    end of exercise 2

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,17 +3,28 @@ import React from "react";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 
-import GuessImport from "../GuessInput";
+import GuessInput from "../GuessInput";
+import GuessResults from "../GuessResults";
 
-// Pick a random word on every pageload.
 const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
+
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuess(tentativeGuess) {
+    const nextGuess = {
+      value: tentativeGuess,
+      id: `${tentativeGuess}-${Math.random()}`,
+    };
+    setGuesses([...guesses, nextGuess]);
+  }
+
   return (
     <>
-      <GuessImport />
+      <GuessResults guesses={guesses} />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
 }
